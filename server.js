b084806dca6dbcea33123d152f7742d808e90791
Diksha Sharma <diksha@softobiz.com>
2023-04-25 15:04:06 +0530

    .env created

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./src/routes/userRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./src/routes/userRoutes");
+const {config} = require('dotenv')
+config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 mongoose
-  .connect('mongodb+srv://diksha170280411:diksha170280@cluster1.7rmns9c.mongodb.net/test', {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log('Failed to connect to MongoDB', err);
+    console.log("Failed to connect to MongoDB", err);
   });
 
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
